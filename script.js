25e4fde875964f7b4216f41b0ebdc4429c190fc8
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-25 00:09:06 +0800

    added start menu

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("test")
+const difficulties = document.querySelectorAll(".difficulty li")
+const cards = document.querySelector(".cards")
+const startBtn = document.querySelector(".start")
+const startMenu = document.querySelector(".start-menu")
+
+let selectedDifficulty = ""
+
+difficulties.forEach(el => {
+  el.addEventListener("click", (e) => {
+    difficulties.forEach(rm => rm.classList.remove("selected"))
+    e.target.classList.add("selected")
+    selectedDifficulty = e.target.textContent
+  })
+})
+
+startBtn.addEventListener("click", startGame)
+
+function startGame() {
+  if(!selectedDifficulty) return
+  startMenu.classList.add("hidden")
+  cards.classList.remove("hidden")
+}
+
+
+function selectCard() {
+
+}
+
+function checkMatch() {
+
+}
