c7b744703e6c08f64128c1be4d59440953c08182
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-26 19:12:13 +0800

    added generate cards

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const difficulties = document.querySelectorAll(".difficulty li")
 const cardCover = document.querySelector(".cards")
-const cards = document.querySelectorAll(".card")
+const cardsClick = document.querySelector("div")
 const startBtn = document.querySelector(".start")
 const startMenu = document.querySelector(".start-menu")
 
 let selected = []
 let disableClick = false
 
+
+
+const cardData = [
+  {
+    id: 0,
+    link: "https://fastly.picsum.photos/id/1/100/150.jpg?hmac=hFr14POiqU2CtVuGXnBN41RjHEbmQcxz0p4GA3stTXw"
+  }, 
+  {
+    id: 1,
+    link: "https://fastly.picsum.photos/id/974/100/150.jpg?hmac=3Bz8zeyeLMZ-Q8SSAHT_1HXiU2y8QQKl1vUGForuMiw"
+  }, 
+  {
+    id: 2,
+    link: "https://fastly.picsum.photos/id/621/100/150.jpg?hmac=iKfuuUf2YMCJsWvHcZZ-VuomRnZr79E-Wuxbb1UqxnU"
+  }, 
+  {
+    id: 3,
+    link: "https://fastly.picsum.photos/id/893/100/150.jpg?hmac=bE57GdTSISElhZaLhSG5t9WoKrbiPBqsvI8J7Q7tC7g"
+  }, 
+  {
+    id: 4,
+    link: "https://fastly.picsum.photos/id/419/100/150.jpg?hmac=iN50Q3xers16MJm6MJSr_8oiDNCbekLmeIIzeGUBsdU"
+  }, 
+  {
+    id: 5,
+    link: "https://fastly.picsum.photos/id/894/100/150.jpg?hmac=QCZQypggIDDR-qEqg-0ZT32CsofVVU4EzHVEcPMeNFo"
+  }
+]
 
 let selectedDifficulty = ""
 
@@ -19,19 +47,29 @@ difficulties.forEach(el => {
 })
 
 startBtn.addEventListener("click", startGame)
-
-cards.forEach(el => {
-  el.addEventListener("click", selectCard)
-})
+cardsClick.addEventListener("click", selectCard)
 
 function startGame() {
   if(!selectedDifficulty) return
+  generateCards(cardData).forEach(el => {
+    const newDiv = document.createElement("div")
+    console.log(el.link)
+    newDiv.innerHTML = `
+      <div class="rounded-md card selected" data-value="${el.id}">
+      <div class="cover"></div>
+      </div>
+    `
+    newDiv.style.background = `url(${el.link})`
+    newDiv.className = "rounded-md"
+    cardCover.appendChild(newDiv)
+  })
   startMenu.classList.add("hidden")
   cardCover.classList.remove("hidden")
 }
 
 
 function selectCard(e) {
+  if(!e.target.closest(".card")) return
   if(selected.length == 2 || disableClick) return
 
   selected.push(e.target.attributes["data-value"].value)
@@ -55,6 +93,7 @@ function checkMatch() {
 }
 
 function hideCards() {
+  const cards = document.querySelectorAll(".card")
   cards.forEach(el => {
     if(selected.find(card => card == el.attributes["data-value"].value)) {
       el.classList.add("selected")
@@ -65,4 +104,10 @@ function hideCards() {
 
 function shuffleCards() {
 
+}
+
+function generateCards(cards) {
+  // randomize the array here
+  const newCards = [...cards, ...cards]
+  return newCards
 }
